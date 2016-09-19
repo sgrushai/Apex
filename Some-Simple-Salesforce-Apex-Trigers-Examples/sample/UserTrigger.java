b2a339c33dfd4d0b4c011a7ad6db69c19941f748
sgrushai <sergii.grushai@gmail.com>
2016-09-19 19:00:21 +0300

    Delete UserTrigger.java
    not relevant

--- Some-Simple-Salesforce-Apex-Trigers-Examples/sample/UserTrigger.java
+++
@@ -1,6 +0,0 @@
-// tips one of the fields (checkbox) on an object
-trigger UserTrigger on User (before insert) {
-    for (User userInLoop : Trigger.new) {
-    userInLoop.ForecastEnabled = true;
-    }
-}
