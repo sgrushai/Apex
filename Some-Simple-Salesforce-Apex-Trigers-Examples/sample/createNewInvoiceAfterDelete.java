66f5627429e2ae44138fac8086b3fe1c304b3344
sgrushai <sergii.grushai@gmail.com>
2016-09-19 18:59:59 +0300

    Delete createNewInvoiceAfterDelete.java
    not relevant

--- Some-Simple-Salesforce-Apex-Trigers-Examples/sample/createNewInvoiceAfterDelete.java
+++
@@ -1,7 +0,0 @@
-// Creates new invoice obj when user deletes the current one
-trigger createNewInvoiceAfterDelete on Invoice__c (after delete) {
-     Invoice__c inv = new Invoice__c(Description__c='My new invoice', Status__c='trigger');
-// Insert the invoice using DML.
-    insert inv;
-   
-}
